bac79bd96167195a7553db56af0d3a751cb4e7f5
Will Neal <willwneal@gmail.com>
2022-05-19 09:05:43 -0400

    Initial file of leetcode solutions to be divide, explicated and added to

---
+++ leetcodeProblems.js
@@ -0,0 +1,40 @@
+// const listA = []
+// const listB = [0]
+
+
+// var mergeTwoLists = function(list1, list2) {
+//     const mergedList = []
+//     for (i=0; i<list1.length; i++) {
+//         if (list1[i] < list2[i]) {
+//             mergedList.push(list1[i])
+//             mergedList.push(list2[i])
+//         } else {
+//             mergedList.push(list2[i])
+//             mergedList.push(list1[i])
+//         }
+//     }
+//     return mergedList
+// };
+
+// mergeTwoLists(listA, listB)
+// console.log(mergeTwoLists(listA, listB))
+
+// const nums = [1, 2, 1]
+
+
+// var getConcatenation = function(arr) {
+//     const ans = []
+//     const sec = []
+//     for (i=0; i<arr.length; i++) {
+//         ans.push(arr[i]);
+//         sec.push(arr[i])
+//     }
+//     for (i=0; i<sec.length; i++) {
+//         ans.push(sec[i])
+//     }
+//     return ans
+// }
+
+// console.log(getConcatenation(nums))
+
+//BUILD ARRAY FROM PERMUTATION 
